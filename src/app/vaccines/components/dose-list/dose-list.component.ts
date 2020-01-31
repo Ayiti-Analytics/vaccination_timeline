70b3e42d91b2cc006e56ee358a9b5cc5b1125a90
Yvel Marcelin <marvelfdsamazon@gmail.com>
2020-01-31 01:09:10 -0500

    Build Commit R.1.0.0

--- src/app/vaccines/components/dose-list/dose-list.component.ts
+++ src/app/vaccines/components/dose-list/dose-list.component.ts
@@ -14,6 +14,7 @@ import { DeviceDetectorService } from 'ngx-device-detector';
 export class DoseListComponent implements OnInit {
   items: Observable<any[]>;
   public innerWidth: any;
+  p: any
   deviceInfo = null;
   isMobile: boolean = false;
   hide: any
